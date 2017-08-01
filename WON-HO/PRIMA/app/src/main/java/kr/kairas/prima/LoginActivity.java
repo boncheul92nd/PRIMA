bea2dbc9b6f3f17fb11f1a87c087596a70afbf82
WonHo Bark <wonho.park83@gmail.com>
2017-08-01 11:53:49 +0900

    login sign 연결

---
+++ WON-HO/PRIMA/app/src/main/java/kr/kairas/prima/LoginActivity.java
@@ -0,0 +1,17 @@
+package kr.kairas.prima;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by karas on 2017-08-01.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ WON-HO/PRIMA/app/src/main/java/kr/kairas/prima/SignupActivity.java
@@ -0,0 +1,16 @@
+package kr.kairas.prima;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by karas on 2017-08-01.
+ */
+
+public class SignupActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
