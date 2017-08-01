6cbadacbbb5c9818585c917594fc0f4eb573c3dd
WonHo Bark <wonho.park83@gmail.com>
2017-08-01 15:26:38 +0900

    list post 연결

--- WON-HO/PRIMA/app/src/main/java/kr/kairas/prima/MenuActivity.java
+++ WON-HO/PRIMA/app/src/main/java/kr/kairas/prima/MenuActivity.java
@@ -1,7 +1,11 @@
 package kr.kairas.prima;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import java.util.List;
 
 /**
  * Created by karas on 2017-08-01.
@@ -14,4 +18,15 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
     }
+
+    public void post(View v) {
+        Intent intent = new Intent(MenuActivity.this, PostActivity.class);
+        startActivity(intent);
+    }
+
+    public void list(View v) {
+        Intent intent = new Intent(MenuActivity.this, ListActivity.class);
+        startActivity(intent);
+    }
+
 }
--- WON-HO/PRIMA/app/src/main/java/kr/kairas/prima/SignupActivity.java
+++ WON-HO/PRIMA/app/src/main/java/kr/kairas/prima/SignupActivity.java
@@ -2,6 +2,7 @@ package kr.kairas.prima;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by karas on 2017-08-01.
@@ -12,5 +13,9 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+
+    public void cancle(View v) {
+        finish();
     }
 }
