1cae3a85448419a6d20314b4ac46ab86358267a6
BonCheul <bonchecul92nd@naver.com>
2017-08-19 16:23:24 +0900

    회원가입 기능 추가

--- BON-CHEUL/demo01/Server.js
+++ BON-CHEUL/demo01/Server.js
@@ -6,7 +6,7 @@ var express = require('express');
 var multer = require('multer');
 var app = express();
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/Car');
+mongoose.connect('mongodb://localhost/PRIMA');
 var fs = require('fs');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -15,23 +15,16 @@ db.once('open', function () {
 });
 var Car = require('./models/Car');
 var Photo = require('./models/Photo');
-var imageSchema = mongoose.Schema({
-    img: {data:Buffer, contentType:String}
-});
-var Item = mongoose.model('Photo', imageSchema);
-
+var User = require('./models/User');
+var logedIn = false;
 
 app.use(multer({
     dest: './uploads/',
     rename: function (fieldname, filename) {
-        return filename;
+        return Date.now();
     },
-    onFileUploadStart: function (file) {
-        // console.log(file.originalname + ' is starting ...')
-    },
-    onFileUploadComplete: function (file) {
-        // console.log(file.fieldname + ' uploaded to  ' + file.path)
-    }
+    onFileUploadStart: function (file) {},
+    onFileUploadComplete: function (file) {}
 }));
 
 app.set('views', './views');
@@ -39,19 +32,112 @@ app.set('view engine', 'jade');
 
 app.get('/', function (req, res) {
     Car.find({}).exec(function (err, result) {
-        console.log(result);
-        res.render('index', {result:result});
+        if(logedIn) {
+            res.render('index', {result: result});
+        } else {
+            res.render('login');
+        }
     });
 });
+
+app.get('/logout', function (req, res) {
+    logedIn = false;
+    res.redirect('/')
+});
+app.post('/login', function (req, res) {
+    var id = req.body.username;
+    var pwd = req.body.password;
+
+    User.findOne({'username':id}).exec(function (err, matchedID) {
+        if (matchedID) {
+            matchedID.comparePassword(pwd, function (err, matchedPWD) {
+                if(matchedPWD) {
+                    logedIn = true;
+                    res.redirect('/');
+                } else {
+                    var output = `
+                    <script type="text/javascript">
+                        alert("비밀번호를 틀리셨습니다.");
+                        history.back();
+                    </script>
+                    `;
+                    res.send(output);
+                }
+            });
+        } else {
+            var output = `
+            <script type="text/javascript">
+                alert("존재하지 않는 아이디입니다.");
+                history.back();
+            </script>
+            `;
+            res.send(output);
+        }
+    });
+});
+
+app.get('/register', function (req, res) {
+    res.render('register');
+});
+app.post('/register', function (req, res) {
+
+    var username = req.body.username;
+    var password = req.body.password;
+
+    dupleIDCheck(username, password, res);
+});
+
+function dupleIDCheck(username, password, res) {
+    User.findOne({'username':username}).exec(function (err, MatchedID) {
+        if(MatchedID) {
+            var output = `
+                <script type="text/javascript">
+                    alert('이미 존재하는 아이디입니다.');
+                    history.back();
+                </script>
+            `;
+            res.send(output);
+            return false;
+        } else {
+            var newUser = new User();
+            newUser.username = username;
+            newUser.password = password;
+            newUser.save(function (err, result) {
+                if(err) {
+                    res.send('error saving User');
+                } else {
+                    console.log(result);
+                    res.render('welecome');
+                }
+            });
+       }
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.get('/input', function (req, res) {
     res.render('input');
 });
 app.get('/record', function (req, res) {
-    res.send('123');
+    Photo.find({}).select('_id').exec(function (err, result) {
+       res.render('record', {result:result});
+    });
 });
 
-app.post('/api/photo', function (req, res) {
-
+app.post('/input', function (req, res) {
     var newCar = new Car();
     var date = req.body.date;
     var location = req.body.location;
@@ -65,7 +151,7 @@ app.post('/api/photo', function (req, res) {
         if(err) {
             res.send('error saving Car');
         } else {
-            console.log(result);
+            console.log(result._id);
         }
     });
 
@@ -76,8 +162,7 @@ app.post('/api/photo', function (req, res) {
         if(err) {
             res.send('error saving Photo');
         } else {
-
-            console.log(result);
+            console.log(result._id);
         }
     });
     res.redirect('/');
