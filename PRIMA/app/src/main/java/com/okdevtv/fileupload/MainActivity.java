3401e09bfe03fef4a11c4316e8fdca274d4892ae
WonHo Bark <wonho.park83@gmail.com>
2017-11-07 22:24:59 +0900

    안드로이드 기초구현 완료, 논문 3.1안드로이드의 구현 업로드

---
+++ PRIMA/app/src/main/java/com/okdevtv/fileupload/MainActivity.java
@@ -0,0 +1,412 @@
+package com.okdevtv.fileupload;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int PICK_FROM_CAMERA = 0;
+    private static final int PICK_FROM_ALBUM = 1;
+    private static final int CROP_FROM_IMAGE = 2;
+
+    private ImageButton imageButton1;
+    private ImageButton imageButton2;
+    private ImageButton imageButton3;
+    private ImageButton imageButton4;
+    private ImageButton imageButton5;
+
+    private TextView script;
+
+    private TextView messageText;
+    private Button posting;
+
+    private Uri mImageCaptureUri;
+    private int id_view;
+    private String absolutePath;
+
+    int serverResponseCode = 0;
+    ProgressDialog dialog = null;
+    String upLoadServerUri = "http://kairas.iptime.org:8083/input";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageButton1 = (ImageButton) this.findViewById(R.id.imageButton1);
+        imageButton2 = (ImageButton) this.findViewById(R.id.imageButton2);
+        imageButton3 = (ImageButton) this.findViewById(R.id.imageButton3);
+        imageButton4 = (ImageButton) this.findViewById(R.id.imageButton4);
+        imageButton5 = (ImageButton) this.findViewById(R.id.imageButton5);
+
+        script = (TextView) this.findViewById(R.id.script);
+
+        messageText = (TextView) this.findViewById(R.id.messageText);
+        posting = (Button) this.findViewById(R.id.posting);
+
+        posting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (imageButton1 == null)
+                    return;
+
+                dialog = ProgressDialog.show(MainActivity.this, "", "Uploading file...", true);
+
+                if (uploadFile(absolutePath) != 0) {
+                    Toast.makeText(MainActivity.this, "사진 전송이 완료되었습니다.", Toast.LENGTH_SHORT).show();
+                }
+
+                new Thread(new Runnable() {
+                    public void run() {
+                        runOnUiThread(new Runnable() {
+                            public void run() {
+                                messageText.setText("uploading started.....");
+                            }
+                        });
+
+                        uploadFile(absolutePath);
+
+                    }
+                }).start();
+            }
+        });
+    }
+
+    public void onClick(View v) {
+        id_view = v.getId();
+
+        if (id_view == R.id.imageButton1 ||
+                id_view == R.id.imageButton2 ||
+                id_view == R.id.imageButton3 ||
+                id_view == R.id.imageButton4 ||
+                id_view == R.id.imageButton5) {
+            new AlertDialog.Builder(this)
+                    .setTitle("업로드할 이미지 선택")
+                    .setPositiveButton("사진촬영", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            doTakePhotoAction();
+                        }
+                    })
+                    .setNeutralButton("앨범선택", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    doTakeAlbumAction();
+                }
+            })
+                    .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .show();
+
+        } else if (id_view == R.id.cancleButton1) {
+            imageButton1.setImageBitmap(null);
+        } else if (id_view == R.id.cancleButton2) {
+            imageButton2.setImageBitmap(null);
+        } else if (id_view == R.id.cancleButton3) {
+            imageButton3.setImageBitmap(null);
+        } else if (id_view == R.id.cancleButton4) {
+            imageButton4.setImageBitmap(null);
+        } else if (id_view == R.id.cancleButton5) {
+            imageButton5.setImageBitmap(null);
+        }
+    }
+
+    public void doTakePhotoAction() // 카메라 촬영 후 이미지 가져오기
+    {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // 임시로 사용할 파일의 경로를 생성
+        String url = "tmp_" + String.valueOf(System.currentTimeMillis()) + ".jpg";
+        mImageCaptureUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(), url));
+
+        intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, mImageCaptureUri);
+        startActivityForResult(intent, PICK_FROM_CAMERA);
+    }
+
+    public void doTakeAlbumAction( ) // 앨범에서 이미지 가져오기
+    {
+        // 앨범 호출
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(intent, PICK_FROM_ALBUM);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode != RESULT_OK)
+            return;
+
+        switch (requestCode) {
+            case PICK_FROM_ALBUM: {
+                // 이후의 처리가 카메라와 같으므로 일단  break없이 진행합니다.
+                // 실제 코드에서는 좀더 합리적인 방법을 선택하시기 바랍니다.
+                mImageCaptureUri = data.getData();
+                Log.d("PRIMA", mImageCaptureUri.getPath().toString());
+            }
+
+            case PICK_FROM_CAMERA: {
+                // 이미지를 가져온 이후의 리사이즈할 이미지 크기를 결정합니다.
+                // 이후에 이미지 크롭 어플리케이션을 호출하게 됩니다.
+                Intent intent = new Intent("com.android.camera.action.CROP");
+                intent.setDataAndType(mImageCaptureUri, "image/*");
+
+                // CROP할 이미지를 200*200 크기로 저장
+                intent.putExtra("outputX", 200); // CROP한 이미지의 x축 크기
+                intent.putExtra("outputY", 200); // CROP한 이미지의 y축 크기
+                intent.putExtra("aspectX", 1); // CROP 박스의 X축 비율
+                intent.putExtra("aspectY", 1); // CROP 박스의 Y축 비율
+                intent.putExtra("scale", true);
+                intent.putExtra("return-data", true);
+                startActivityForResult(intent, CROP_FROM_IMAGE); // CROP_FROM_CAMERA case문 이동
+                break;
+            }
+
+            case CROP_FROM_IMAGE: {
+                // 크롭이 된 이후의 이미지를 넘겨 받습니다.
+                // 이미지뷰에 이미지를 보여준다거나 부가적인 작업 이후에
+                // 임시 파일을 삭제합니다.
+                if (resultCode != RESULT_OK) {
+                    return;
+                }
+
+                final Bundle extras = data.getExtras();
+
+                // CROP된 이미지를 저장하기 위한 FILE 경로
+                String filePath = Environment.getExternalStorageDirectory().getAbsolutePath() +
+                        "/DCIM/PRIMA/" + System.currentTimeMillis() + ".jpg";
+
+                if (extras != null) {
+                    Bitmap photo = extras.getParcelable("data"); // CROP된 BITMAP
+
+                    if ( id_view == R.id.imageButton1) {
+                        imageButton1.setImageBitmap(photo); // 레이아웃의 이미지칸에 CROP된 BITMAP을 보여줌
+                    } else if (id_view == R.id.imageButton2) {
+                        imageButton2.setImageBitmap(photo); // 레이아웃의 이미지칸에 CROP된 BITMAP을 보여줌
+                    } else if (id_view == R.id.imageButton3) {
+                        imageButton3.setImageBitmap(photo); // 레이아웃의 이미지칸에 CROP된 BITMAP을 보여줌
+                    } else if (id_view == R.id.imageButton4) {
+                        imageButton4.setImageBitmap(photo); // 레이아웃의 이미지칸에 CROP된 BITMAP을 보여줌
+                    } else if (id_view == R.id.imageButton5) {
+                        imageButton5.setImageBitmap(photo); // 레이아웃의 이미지칸에 CROP된 BITMAP을 보여줌
+                    }
+
+                    storeCropImage(photo, filePath); // CROP된 이미지를 외부저장소, 앨범에 저장한다.
+                    absolutePath = filePath;
+                    break;
+
+                }
+                // 임시 파일 삭제
+                File f = new File(mImageCaptureUri.getPath());
+                if (f.exists()) {
+                    f.delete();
+                }
+            }
+        }
+    }
+
+    private void storeCropImage(Bitmap bitmap, String filePath) {
+        String dirPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/DCIM/PRIMA";
+        File prima = new File(dirPath);
+        if (!prima.exists()) // PRIMA 디렉터리에 폴더가 없다면 (새로 이미지를 저장할 경우에 속한다.)
+            prima.mkdir();
+
+        File copyFile = new File(filePath);
+        BufferedOutputStream out = null;
+
+        try {
+            copyFile.createNewFile();
+            out = new BufferedOutputStream(new FileOutputStream(copyFile));
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+
+            sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE,
+                    Uri.fromFile(copyFile)));
+
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.d("StoreCropImage", e.toString());
+        }
+    }
+
+    public int uploadFile(String sourceFileUri) {
+
+        String fileName = sourceFileUri;
+
+        HttpURLConnection conn = null;
+        DataOutputStream dos = null;
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1 * 1024 * 1024;
+        File sourceFile = new File(sourceFileUri);
+
+        if (!sourceFile.isFile()) {
+
+            dialog.dismiss();
+
+            Log.e("uploadFile", "Source File not exist :"
+                    + absolutePath);
+
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    messageText.setText("Source File not exist :"
+                            + absolutePath);
+                }
+            });
+
+            return 0;
+
+        } else {
+            try {
+
+                // open a URL connection to the Servlet
+                FileInputStream fileInputStream = new FileInputStream(sourceFile);
+                URL url = new URL(upLoadServerUri);
+
+                // Open a HTTP  connection to  the URL
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setDoInput(true); // Allow Inputs
+                conn.setDoOutput(true); // Allow Outputs
+                conn.setUseCaches(false); // Don't use a Cached Copy
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Connection", "Keep-Alive");
+                conn.setRequestProperty("ENCTYPE", "multipart/form-data");
+                conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+                conn.setRequestProperty("uploaded_file", fileName);
+
+                dos = new DataOutputStream(conn.getOutputStream());
+
+                dos.writeBytes(twoHyphens + boundary + lineEnd);
+                dos.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\""
+                        + fileName + "\"" + lineEnd);
+
+                dos.writeBytes(lineEnd);
+
+                // create a buffer of  maximum size
+                bytesAvailable = fileInputStream.available();
+
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                buffer = new byte[bufferSize];
+
+                // read file and write it into form...
+                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                while (bytesRead > 0) {
+
+                    dos.write(buffer, 0, bufferSize);
+                    bytesAvailable = fileInputStream.available();
+                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                }
+
+                // send multipart form data necesssary after file data...
+                dos.writeBytes(lineEnd);
+                dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+                // Responses from the server (code and message)
+                serverResponseCode = conn.getResponseCode();
+                String serverResponseMessage = conn.getResponseMessage();
+
+                Log.i("uploadFile", "HTTP Response is : "
+                        + serverResponseMessage + ": " + serverResponseCode);
+
+                if (serverResponseCode == 200) {
+                    BufferedReader input = new BufferedReader(new InputStreamReader(conn.getInputStream()), 8192);
+                    final StringBuilder response = new StringBuilder();
+                    String strLine = null;
+                    while ((strLine = input.readLine()) != null) {
+                        response.append(strLine);
+                    }
+                    input.close();
+
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+
+
+                            messageText.setText("File Upload Completed.");
+
+                            String msg = "File Upload Completed.\n See uploaded file here : \n"
+                                    + response.toString();
+
+
+
+                            script.setText(msg);
+                        }
+                    });
+                }
+
+                //close the streams //
+                fileInputStream.close();
+                dos.flush();
+                dos.close();
+
+            } catch (MalformedURLException ex) {
+
+                dialog.dismiss();
+                ex.printStackTrace();
+
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        messageText.setText("MalformedURLException Exception : check script url.");
+                        Toast.makeText(MainActivity.this, "MalformedURLException",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                Log.e("Upload file to server", "error: " + ex.getMessage(), ex);
+            } catch (Exception e) {
+
+                dialog.dismiss();
+                e.printStackTrace();
+
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        messageText.setText("Got Exception : see logcat ");
+                        Toast.makeText(MainActivity.this, "Got Exception : see logcat ",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+                Log.e("Upload file Exception", "Exception : " + e.getMessage(), e);
+            }
+            dialog.dismiss();
+            return serverResponseCode;
+
+        } // End else block
+    }
+}
